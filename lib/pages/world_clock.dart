00264a97f262a7ad2c575d16d78b63cb04f4db4e
lakindulk <lakindulk9@gmail.com>
2022-03-28 10:58:59 +0530

    worldClock

--- lib/pages/world_clock.dart
+++ lib/pages/world_clock.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'package:clock_app/component/lakindu/clockview.dart';
+import 'package:clock_app/component/lakindu/selectzone.dart';
 import 'package:flutter/material.dart';
 import '../component/navBar.dart';
+import 'package:intl/intl.dart';
 
 class WorldClock extends StatefulWidget {
   static const String routeName = '/worldclock';
@@ -13,13 +16,100 @@ class WorldClock extends StatefulWidget {
 class _WorldClockState extends State<WorldClock> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedDate = DateFormat('EEE,d MMM').format(now);
     return Scaffold(
       appBar: AppBar(
         title: const Text("WorldClock"),
       ),
-      body: Clockview(),
-      
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                child: Container(child: Clockview()),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const DigitalClockWidget(),
+                    Text(
+                      formattedDate,
+                      style: const TextStyle(
+                          fontFamily: 'avenir',
+                          fontWeight: FontWeight.w300,
+                          color: Colors.grey,
+                          fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: MaterialButton(
+              color: Colors.cyan,
+              shape: CircleBorder(),
+              child: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Selectzone()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: const NavBar(currItem: 0),
+    );
+  }
+}
+
+class DigitalClockWidget extends StatefulWidget {
+  const DigitalClockWidget({
+    Key? key,
+  }) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return DigitalClockWidgetState();
+  }
+}
+
+class DigitalClockWidgetState extends State<DigitalClockWidget> {
+  var formattedTime = DateFormat('hh mm').format(DateTime.now());
+  late Timer timer;
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      var perviousMinute =
+          DateTime.now().add(const Duration(seconds: -1)).minute;
+      var currentMinute = DateTime.now().minute;
+      if (perviousMinute != currentMinute) {
+        setState(() {
+          formattedTime = DateFormat('hh mm').format(DateTime.now());
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      formattedTime,
+      style: const TextStyle(
+          fontFamily: 'avenir', color: Colors.grey, fontSize: 68),
     );
   }
 }
